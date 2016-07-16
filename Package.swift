69a98aa22e12fb467f9144571309bb798b908269
Andrey Fidrya <af@zabiyaka.com>
2016-07-16 17:12:00 +0300

    Add CEvent and ConfigFile imports

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "Smud"
+    name: "Smud",
+    dependencies: [
+	    .Package(url: "https://github.com/smud/CEvent.git", majorVersion: 0),
+	    .Package(url: "https://github.com/smud/ConfigFile.git", majorVersion: 0),
+    ]
 )
+
