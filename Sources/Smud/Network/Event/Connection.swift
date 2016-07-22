4117cf820b22d663fccc67a863c2a6cbb7810217
Andrey Fidrya <af@zabiyaka.com>
2016-07-22 13:56:14 +0300

    Connection with send() and readLine(). Smud now works as echo server

---
+++ Sources/Smud/Network/Event/Connection.swift
@@ -0,0 +1,75 @@
+//
+// Connection.swift
+//
+// This source file is part of the SMUD open source project
+//
+// Copyright (c) 2016 SMUD project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SMUD project authors
+//
+
+import Foundation
+import CEvent
+
+struct Connection {
+    let bufferEvent: OpaquePointer
+    
+    /// Sends the textual representations of `items`, separated by
+    /// `separator` and terminated by `terminator` to the remote endpoint.
+    ///
+    /// The textual representations are obtained for each `item` via
+    /// the expression `String(item)`.
+    ///
+    /// According to RFC 1123, the Telnet end-of-line sequence CR LF
+    /// MUST be used to send Telnet data that is not terminal-to-computer
+    /// (e.g., for Server Telnet sending output, or the Telnet protocol
+    /// incorporated another application protocol). `rfc1123EOLs` option
+    /// converts all LFs to CR LF pairs.
+    ///
+    /// - Note: To print without a trailing newline, pass `terminator: ""`
+    ///
+    /// - SeeAlso: `debugPrint`, `Streamable`, `CustomStringConvertible`,
+    ///   `CustomDebugStringConvertible`
+    func send(_ items: Swift.Any..., separator: String = "", terminator: String = "\n", rfc1123EOLs: Bool = true) {
+        let output = bufferevent_get_output(bufferEvent)
+
+        let separatorProcessed = rfc1123EOLs ? separator.replacingOccurrences(of: "\n", with: "\r\n") : separator
+        let separatorUtf8 = separatorProcessed.utf8
+        let separatorData = [UInt8](separatorUtf8)
+        let separatorUtf8Count = separatorUtf8.count
+        
+        var isFirst = true
+        for item in items {
+            if !isFirst && !separator.isEmpty {
+                evbuffer_add(output, separatorData, separatorUtf8Count)
+            }
+            let text = String(item)
+            let textProcessed = rfc1123EOLs ? text.replacingOccurrences(of: "\n", with: "\r\n") : text
+            let textUtf8 = textProcessed.utf8
+            let data = [UInt8](textUtf8)
+            evbuffer_add(output, data, textUtf8.count)
+            isFirst = false
+        }
+
+        if !terminator.isEmpty {
+            let terminatorProcessed = rfc1123EOLs ? terminator.replacingOccurrences(of: "\n", with: "\r\n") : terminator
+            let terminatorUtf8 = terminatorProcessed.utf8
+            let terminatorData = [UInt8](terminatorUtf8)
+            evbuffer_add(output, terminatorData, terminatorUtf8.count)
+        }
+    }
+    
+    func readLine() -> String? {
+        let input = bufferevent_get_input(bufferEvent)
+
+        var bytesRead: Int = 0
+        let result = evbuffer_readln(input, &bytesRead, EVBUFFER_EOL_CRLF)
+        guard let cString = result else {
+            return nil
+        }
+        defer { free(cString) }
+        return String(cString: cString)
+    }
+}
--- Sources/Smud/Network/Event/ConnectionListener.swift
+++ Sources/Smud/Network/Event/ConnectionListener.swift
@@ -69,7 +69,11 @@ class ConnectionListener {
         }
         
         let base = evconnlistener_get_base(listener)
-        let bev = bufferevent_socket_new(base, fd, Int32(BEV_OPT_CLOSE_ON_FREE.rawValue))
+        let bevSocket = bufferevent_socket_new(base, fd, Int32(BEV_OPT_CLOSE_ON_FREE.rawValue))
+        guard let bev = bevSocket else {
+            print("Unable to accept connection")
+            return
+        }
         
         let context = unsafeBitCast(self, to: UnsafeMutablePointer<Void>.self)
         bufferevent_setcb(bev,
@@ -87,6 +91,9 @@ class ConnectionListener {
             },
             /* cbarg */ context)
         bufferevent_enable(bev, Int16(EV_READ)|Int16(EV_WRITE))
+
+        let connection = Connection(bufferEvent: bev)
+        connection.send("Hello")
     }
     
     func onAcceptError(listener: OpaquePointer?) {
@@ -100,6 +107,14 @@ class ConnectionListener {
     
     func onRead(bev: OpaquePointer?) {
         print("onRead")
+        guard let bev = bev else { return }
+        //let input = bufferevent_get_input(bev)
+        //let output = bufferevent_get_output(bev)
+        let connection = Connection(bufferEvent: bev)
+        while let line = connection.readLine() {
+            print("Got line: \(line)")
+            connection.send("Got line: \(line)")
+        }
     }
 
     func onWrite(bev: OpaquePointer?) {
@@ -108,5 +123,14 @@ class ConnectionListener {
 
     func onEvent(bev: OpaquePointer?, events: Int16) {
         print("onEvent")
+        if 0 != events & Int16(BEV_EVENT_ERROR) {
+            perror("Error from bufferevent")
+        }
+        if 0 != events & Int16(BEV_EVENT_EOF) {
+            print("Connection closed by remote")
+        }
+        if (0 != events & (Int16(BEV_EVENT_EOF) | Int16(BEV_EVENT_ERROR))) {
+            bufferevent_free(bev);
+        }
     }
 }
