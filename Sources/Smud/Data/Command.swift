1da465101d1c231db3acf62579329cbc6dd2751a
Andrey Fidrya <af@zabiyaka.com>
2016-12-10 04:30:50 +0300

    Add missing imports

--- Sources/Smud/Data/Command.swift
+++ Sources/Smud/Data/Command.swift
@@ -11,6 +11,7 @@
 //
 
 import Foundation
+import StringUtils
 
 public class Command {
     typealias T = Command
--- Sources/Smud/Data/Tag.swift
+++ Sources/Smud/Data/Tag.swift
@@ -11,6 +11,7 @@
 //
 
 import Foundation
+import StringUtils
 
 public class Tag: CustomStringConvertible {
     var area: String?
