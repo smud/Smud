4685c03c881fa2f50723866ea1381b096df9519e
Anton Karandeev <tonygluk@gmail.com>
2017-01-26 02:30:06 +0300

    Fix and unify area map shifting

--- Sources/Smud/Data/AreaMap.swift
+++ Sources/Smud/Data/AreaMap.swift
@@ -19,192 +19,100 @@ public class AreaMap {
         case didAddRoom
         case didNothing
     }
-    
+
     var mapElementsByPosition = [AreaMapPosition: AreaMapElement]()
     var positionsByRoom = [Room: AreaMapPosition]()
-    
+
     var elementsCount: Int { return mapElementsByPosition.count }
     var roomsCount: Int { return positionsByRoom.count }
-    var xRange = 0 ..< 0
-    var yRange = 0 ..< 0
-    var planeRange = 0 ..< 0
-    
+    var range = (from: AreaMapPosition(0, 0, 0), to: AreaMapPosition(0, 0, 0))
+
     init(startingRoom: Room? = nil) {
         if let startingRoom = startingRoom {
             let toPosition = AreaMapPosition(0, 0, 0)
             add(room: startingRoom, position: toPosition)
         }
     }
-    
+
     func dig(toRoom: Room, fromRoom: Room, direction: Direction) -> DigResult {
         guard positionsByRoom[toRoom] == nil else { return .toRoomAlreadyExists }
         guard let fromPosition = positionsByRoom[fromRoom] else { return .fromRoomDoesNotExist }
-        switch direction {
-        case .north:
-            let toPosition = fromPosition.adjustedBy(y: -1)
-            if mapElementsByPosition[toPosition] != nil {
-                shift(northOf: fromPosition.y, distance: 1)
-            }
-            add(room: toRoom, position: toPosition)
-            return .didAddRoom
-        case .south:
-            let toPosition = fromPosition.adjustedBy(y: 1)
-            if mapElementsByPosition[toPosition] != nil {
-                shift(southOf: fromPosition.y, distance: 1)
-            }
-            add(room: toRoom, position: toPosition)
-            return .didAddRoom
-        case .west:
-            let toPosition = fromPosition.adjustedBy(x: -1)
-            if mapElementsByPosition[toPosition] != nil {
-                shift(westOf: fromPosition.x, distance: 1)
-            }
-            add(room: toRoom, position: toPosition)
-            return .didAddRoom
-        case .east:
-            let toPosition = fromPosition.adjustedBy(x: 1)
-            if mapElementsByPosition[toPosition] != nil {
-                shift(eastOf: fromPosition.x, distance: 1)
-            }
-            add(room: toRoom, position: toPosition)
-            return .didAddRoom
-        case .up:
-            // TODO
-            break
-        case .down:
-            // TODO
-            break
+
+        let toPosition = fromPosition + AreaMapPosition(direction,  1)
+        if mapElementsByPosition[toPosition] != nil {
+            shift(from: fromPosition, direction: direction, distance: 1)
         }
-        return .didNothing
+
+        add(room: toRoom, position: toPosition)
+
+        return .didAddRoom
     }
-    
+
     func add(room: Room, position: AreaMapPosition) {
         mapElementsByPosition[position] = .room(room)
         positionsByRoom[room] = position
-        
-        xRange = min(position.x, xRange.lowerBound) ..< max(position.x + 1, xRange.upperBound)
-        yRange = min(position.y, yRange.lowerBound) ..< max(position.y + 1, yRange.upperBound)
-        planeRange = min(position.plane, planeRange.lowerBound) ..< max(position.plane + 1, planeRange.upperBound)
+
+        range.from = lowerBound(range.from, position)
+        range.to = upperBound(range.to, position)
     }
-    
-    func shift(northOf y: Int, distance: Int) {
-        yRange = yRange.lowerBound - distance ..< yRange.upperBound
-        
+
+    func shift(from: AreaMapPosition, direction: Direction, distance: Int) {
+        let axis = AreaMapPosition.Axis(direction)
+        let shift = AreaMapPosition(direction, distance)
+
+        range.from = lowerBound(range.from, range.from + shift)
+        range.to = upperBound(range.to, range.to + shift)
+
         let oldMapElementsByPosition = mapElementsByPosition
-        
+
         for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.y < y else { continue }
-            mapElementsByPosition.removeValue(forKey: oldPosition)
-            
-            let newPosition = oldPosition.adjustedBy(y: -distance)
+            guard (oldPosition - from).direction(axis: axis) == direction else {
+                mapElementsByPosition[oldPosition] = element
+                continue
+            }
+
+            let newPosition = oldPosition + shift
             mapElementsByPosition[newPosition] = element
             if case .room(let room) = element {
                 positionsByRoom[room] = newPosition
             }
         }
-        
+
+
+        let fillElement = AreaMapElement.passage(axis)
+        let fillFrom = (from + AreaMapPosition(direction, 1)).get(axis: axis)
+        let fillTo = (from + AreaMapPosition(direction, distance)).get(axis: axis)
+        let fillRange = min(fillFrom, fillTo)...max(fillFrom, fillTo)
         for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.y == y - 1 else { continue }
-            if case .passage(.northSouth) = element {
-                for fillY in y - distance - 1 ... y - 1 {
-                    let position = AreaMapPosition(oldPosition.x, fillY, oldPosition.plane)
-                    mapElementsByPosition[position] = element
+            guard oldPosition.get(axis: axis) == from.get(axis: axis) else {
+                continue
+            }
+
+            switch element {
+            case .room(let room) where room.exits[direction] != nil: fallthrough
+            case .passage(axis):
+                for fillCoordinate in fillRange {
+                    var position = oldPosition
+                    position.set(axis: axis, value: fillCoordinate)
+                    mapElementsByPosition[position] = fillElement
                 }
+            default:
+                break
             }
         }
+
     }
 
-    func shift(southOf y: Int, distance: Int) {
-        yRange = yRange.lowerBound ..< yRange.upperBound + distance
-
-        let oldMapElementsByPosition = mapElementsByPosition
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.y > y else { continue }
-            mapElementsByPosition.removeValue(forKey: oldPosition)
-            
-            let newPosition = oldPosition.adjustedBy(y: distance)
-            mapElementsByPosition[newPosition] = element
-            if case .room(let room) = element {
-                positionsByRoom[room] = newPosition
-            }
-        }
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.y == y + 1 else { continue }
-            if case .passage(.northSouth) = element {
-                for fillY in y + 1 ... y + distance + 1 {
-                    let position = AreaMapPosition(oldPosition.x, fillY, oldPosition.plane)
-                    mapElementsByPosition[position] = element
-                }
-            }
-        }
-    }
-
-    func shift(westOf x: Int, distance: Int) {
-        xRange = xRange.lowerBound - distance ..< xRange.upperBound
-
-        let oldMapElementsByPosition = mapElementsByPosition
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.x < x else { continue }
-            mapElementsByPosition.removeValue(forKey: oldPosition)
-            
-            let newPosition = oldPosition.adjustedBy(x: -distance)
-            mapElementsByPosition[newPosition] = element
-            if case .room(let room) = element {
-                positionsByRoom[room] = newPosition
-            }
-        }
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.x == x - 1 else { continue }
-            if case .passage(.westEast) = element {
-                for fillX in x - distance - 1 ... x - 1 {
-                    let position = AreaMapPosition(fillX, oldPosition.y, oldPosition.plane)
-                    mapElementsByPosition[position] = element
-                }
-            }
-        }
-    }
-
-    func shift(eastOf x: Int, distance: Int) {
-        xRange = xRange.lowerBound ..< xRange.upperBound + distance
-
-        let oldMapElementsByPosition = mapElementsByPosition
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.x > x else { continue }
-            mapElementsByPosition.removeValue(forKey: oldPosition)
-            
-            let newPosition = oldPosition.adjustedBy(x: distance)
-            mapElementsByPosition[newPosition] = element
-            if case .room(let room) = element {
-                positionsByRoom[room] = newPosition
-            }
-        }
-        
-        for (oldPosition, element) in oldMapElementsByPosition {
-            guard oldPosition.x == x + 1 else { continue }
-            if case .passage(.westEast) = element {
-                for fillX in x + 1 ... x + distance + 1 {
-                    let position = AreaMapPosition(fillX, oldPosition.y, oldPosition.plane)
-                    mapElementsByPosition[position] = element
-                }
-            }
-        }
-    }
-    
     func debugPrint() {
-        let minX = xRange.lowerBound
-        let minY = yRange.lowerBound
+        let minX = range.from.x
+        let minY = range.from.y
         //let minPlane = planeRange.lowerBound
 
         let elementWidth = 14
 
-        let fillLine = [String](repeating: " ".padding(toLength: elementWidth, withPad: " ", startingAt: 0), count: xRange.count)
-        var grid = [[String]](repeating: fillLine, count: yRange.count)
-        
+        let fillLine = [String](repeating: " ".padding(toLength: elementWidth, withPad: " ", startingAt: 0), count: range.to.x - range.from.x + 1)
+        var grid = [[String]](repeating: fillLine, count: range.to.y - range.from.y + 1)
+
         for (position, element) in mapElementsByPosition {
             guard position.plane == 0 else { continue }
             let atX = position.x - minX
@@ -212,18 +120,18 @@ public class AreaMap {
             switch element {
             case .room(let room):
                 grid[atY][atX] = room.id.padding(toLength: elementWidth, withPad: " ", startingAt: 0)
-            case .passage(let orientation):
-                switch orientation {
-                case .northSouth:
+            case .passage(let axis):
+                switch axis {
+                case .y:
                     grid[atY][atX] = "|".padding(toLength: elementWidth, withPad: " ", startingAt: 0)
-                case .westEast:
+                case .x:
                     grid[atY][atX] = "-".padding(toLength: elementWidth, withPad: " ", startingAt: 0)
-                case .upDown:
+                case .plane:
                     grid[atY][atX] = "x".padding(toLength: elementWidth, withPad: " ", startingAt: 0)
                 }
             }
         }
-        
+
         for row in grid {
             print(row)
         }
--- Sources/Smud/Data/AreaMapPosition.swift
+++ Sources/Smud/Data/AreaMapPosition.swift
@@ -14,6 +14,20 @@
 import Foundation
 
 struct AreaMapPosition: Hashable, Equatable {
+    enum Axis {
+        init(_ direction: Direction) {
+            switch direction {
+            case .north: self = .y
+            case .south: self = .y
+            case .east: self = .x
+            case .west: self =  .x
+            case .up: self = .plane
+            case .down: self = .plane
+            }
+        }
+        case x, y, plane
+    }
+
     var x: Int
     var y: Int
     var plane: Int
@@ -23,7 +37,66 @@ struct AreaMapPosition: Hashable, Equatable {
         self.y = y
         self.plane = plane
     }
-    
+
+    init(_ direction: Direction, _ distance: Int) {
+        switch direction {
+        case .north: self.init(.y, -distance)
+        case .south: self.init(.y, distance)
+        case .west: self.init(.x, -distance)
+        case .east: self.init(.x, distance)
+        case .down: self.init(.plane, -distance)
+        case .up: self.init(.plane, distance)
+        }
+    }
+
+    init(_ axis: Axis, _ value: Int) {
+        switch (axis) {
+        case .x: self.init(value, 0, 0)
+        case .y: self.init(0, value, 0)
+        case .plane: self.init(0, 0, value)
+        }
+    }
+
+    func direction(axis: Axis) -> Direction? {
+        switch axis {
+        case .x:
+            if self.x > 0 {
+                return .east
+            } else if (self.x < 0) {
+                return .west
+            }
+        case .y:
+            if (self.y > 0) {
+                return .south
+            } else if (self.y < 0) {
+                return .north
+            }
+        case .plane:
+            if (self.plane > 0) {
+                return .up
+            } else if (self.plane < 0) {
+                return .down
+            }
+        }
+        return nil
+    }
+
+    func get(axis: Axis) -> Int {
+        switch axis {
+        case .x: return self.x
+        case .y: return self.y
+        case .plane: return self.plane
+        }
+    }
+
+    mutating func set(axis: Axis, value: Int) {
+        switch axis {
+        case .x: self.x = value
+        case .y: self.y = value
+        case .plane: self.plane = value
+        }
+    }
+
     func adjustedBy(x: Int) -> AreaMapPosition {
         return AreaMapPosition(self.x + x, y, plane)
     }
@@ -31,11 +104,12 @@ struct AreaMapPosition: Hashable, Equatable {
     func adjustedBy(y: Int) -> AreaMapPosition {
         return AreaMapPosition(x, self.y + y, plane)
     }
-    
+
     func adjustedBy(plane: Int) -> AreaMapPosition {
         return AreaMapPosition(x, y, self.plane + plane)
     }
-    
+
+
     static func ==(lhs: AreaMapPosition, rhs: AreaMapPosition) -> Bool {
         return lhs.plane == rhs.plane &&
             lhs.x == rhs.x && lhs.y == rhs.y
@@ -50,3 +124,28 @@ struct AreaMapPosition: Hashable, Equatable {
         return result
     }
 }
+
+func +(left: AreaMapPosition, right: AreaMapPosition) -> AreaMapPosition {
+    return AreaMapPosition(left.x + right.x, left.y + right.y, left.plane + right.plane)
+}
+
+func -(left: AreaMapPosition, right: AreaMapPosition) -> AreaMapPosition {
+    return AreaMapPosition(left.x - right.x, left.y - right.y, left.plane - right.plane)
+}
+
+func +=(left: inout AreaMapPosition, right: AreaMapPosition) {
+    left = left + right
+}
+
+func -=(left: inout AreaMapPosition, right: AreaMapPosition) {
+    left = left - right
+}
+
+func lowerBound(_ one: AreaMapPosition, _ other: AreaMapPosition) -> AreaMapPosition {
+    return AreaMapPosition(min(one.x, other.x), min(one.y, other.y), min(one.plane, other.plane))
+}
+
+func upperBound(_ one: AreaMapPosition, _ other: AreaMapPosition) -> AreaMapPosition {
+    return AreaMapPosition(max(one.x, other.x), max(one.y, other.y), max(one.plane, other.plane))
+}
+
