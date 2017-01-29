19373742a387b33f9985322e2ae4bfd0e7a3078f
Anton Karandeev <tonygluk@gmail.com>
2017-01-29 20:29:11 +0300

    Fix license header in AreaMapRange and add contributor

--- Sources/Smud/Data/AreaMapRange.swift
+++ Sources/Smud/Data/AreaMapRange.swift
@@ -1,9 +1,13 @@
 //
-//  AreaMapRange.swift
-//  DemoMUD
+// AreaMapElement.swift
 //
-//  Created by Anton Karandeev on 29/01/2017.
+// This source file is part of the SMUD open source project
 //
+// Copyright (c) 2016 SMUD project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SMUD project authors
 //
 
 import Foundation
